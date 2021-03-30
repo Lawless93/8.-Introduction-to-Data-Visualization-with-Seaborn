653fcf67fd5bb575d5a3cfb244e474c9c39c4ca6
Lawless93 <lawless93@hotmail.com>
2021-03-30 21:08:10 +0100

    UCD Data Analytics - Introduction to Data Visualization with Seaborn

---
+++ Chapter 1 - Introduction to Seaborn.py
@@ -0,0 +1,63 @@
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Change this scatter plot to have percent literate on the y-axis
+sns.scatterplot(x=gdp, y=percent_literate)
+
+# Show plot
+plt.show()
+
+
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Create count plot with region on the y-axis
+sns.countplot(y=region)
+
+# Show plot
+plt.show()
+
+
+# Import Matplotlib, Pandas, and Seaborn
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Create a DataFrame from csv file
+df = pd.read_csv(csv_filepath)
+
+# Create a count plot with "Spiders" on the x-axis
+sns.countplot(x="Spiders", data=df)
+
+# Display the plot
+plt.show()
+
+
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Change the legend order in the scatter plot
+sns.scatterplot(x="absences", y="G3",
+                data=student_data,
+                hue="location",
+                hue_order=["Rural", "Urban"])
+
+# Show plot
+plt.show()
+
+
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Create a dictionary mapping subgroup values to colors
+palette_colors = {"Rural": "green", "Urban": "blue"}
+
+# Create a count plot of school with location subgroups
+sns.countplot(x="school", data=student_data, hue="location", palette=palette_colors)
+
+# Display plot
+plt.show()
